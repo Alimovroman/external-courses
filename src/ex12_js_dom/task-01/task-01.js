dd680d557bf29457fa7ffb0e916386b6567329ad
Roman Alimov <alimov.ramon@gmail.com>
2019-12-01 17:22:55 +0300

    fixed: replace className, position Absolute on flex, add UL/LI in DOM,

--- src/ex12_js_dom/task-01/task-01.js
+++ src/ex12_js_dom/task-01/task-01.js
@@ -1,11 +1,11 @@
 let slider = {
     slides: ['asset/cats.jpg', 'asset/horse.jpg', 'asset/jaguar.jpg', 'asset/kroliki.jpg', 'asset/panda.jpg', 'asset/tigr.jpg'],
     frame: 0,
-    set: function(image) {
+    implant: function(image) {
       document.getElementById("screen").style.backgroundImage = `url( ${image} )`;
     },
     init: function() {
-      this.set(this.slides[this.frame]);
+      this.implant(this.slides[this.frame]);
     },
     left: function() {
       slider.frame--;
@@ -14,7 +14,7 @@ let slider = {
       }
       document.body.style.transform = "skew(20deg, -20deg)";
       setTimeout(() => {document.body.style.transform = ""}, 1000);
-      slider.set(slider.slides[slider.frame]);
+      slider.implant(slider.slides[slider.frame]);
     },
     right: function() {
       slider.frame++;
@@ -23,11 +23,11 @@ let slider = {
       }
       document.body.style.transform = "skew(-20deg, 20deg)";
       setTimeout(() => {document.body.style.transform = ""}, 1000);
-      slider.set(slider.slides[slider.frame]);
+      slider.implant(slider.slides[slider.frame]);
     }
   };
 window.onload = function() {
   slider.init();
 }
-document.querySelector(`.left`).addEventListener(`click`, slider.left);
-document.querySelector(`.right`).addEventListener(`click`, slider.right);
+document.querySelector(`.back`).addEventListener(`click`, slider.left);
+document.querySelector(`.next`).addEventListener(`click`, slider.right);
--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -1,6 +1,11 @@
-let select = document.getElementsByClassName("select");
+let select = document.getElementsByClassName("menuContainer");
+select[0].innerHTML = `<ul class="menu">
+<li class="menuList">My account</li>
+<li class="menuList">My task</li>
+<li class="menuList">Setting</li>
+<li class="menuList">Log out</li>
+</ul>`;
 select[0].hidden = true;
-
 function openList() {
   select[0].hidden = !select[0].hidden;
   document.getElementsByClassName(`arrow`)[0].style.transform = "rotate(-90deg)" ;
@@ -9,3 +14,4 @@ function openList() {
   }
 };
 document.querySelector(`.arrow`).addEventListener(`click`, openList);
+document.querySelector(`.menu`).addEventListener(`click`, openList);
